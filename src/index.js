aae14c061bbbd370dedd5225220af907e1c79dbb
Mark <markgabon@hotmail.com>
2022-02-10 19:54:02 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-const express = require('express')
-const server = express()
 
+const express = require('express')
+const cors = require('cors')
+
+const server = express()
 require('dotenv').config();
 
-const PORT = process.env.PORT || 8000;
-server.listen(PORT, () => {
-    console.log('listening on port', PORT)
+const PORT = process.env.PORT || 8000
+
+server.use(express.json())
+server.use(cors())
+
+server.get('/api/greet', (req, res) => {
+    res.json({message: "api is working"})
 })
 
 server.use('*', (req, res) => {
-    res.send('<h1>Hello!</h1>')
+    res.send('<h1>Welcome!</h1>')
+})
+
+server.use((err, req, res ,next) => {
+res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+})
 })
